694a5d8ba45011b967a27333e62d0208ab733763
William <william.chuang@fusions360.com>
2024-11-01 01:52:00 +0800

    update

--- src/views/Camera.vue
+++ src/views/Camera.vue
@@ -49,6 +49,7 @@
       top="10vh"
     )
       .wrap
+        .videoHidden
         <iframe width="800" height="450" src="https://www.youtube.com/embed/fcoq60SjBIs?si=bN9vohfOIabXQcU1&autoplay=1&rel=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         
 
